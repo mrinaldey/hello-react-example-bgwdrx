82577dc6b2fc86a055c228182d7a70a2da24dc2f
mrinaldey <mrinalsdey33@gmail.com>
2021-12-08 17:44:47 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-const name = 'Riaan';
-const el = <p>I lobh you, {name}</p>;
-const em = <h1>yesh, {el}</h1>;
-ReactDOM.render(em, document.getElementById('root'));
+let counter = 0;
+let show = () => {
+  counter++;
+  const el = <h1>My love for you increases like:{counter}</h1>;
+  ReactDOM.render(el, document.getElementById('root'));
+};
+setInterval(show, 500);
+
+function Confess() {
+  return <h1>I love you.</h1>;
+}
+const dum = <Confess />;
+ReactDOM.render(dum, document.getElementById('chapri'));
