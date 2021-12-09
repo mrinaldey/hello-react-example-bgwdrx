fae041de8d5f456e87e61eba8f90ee9fcab5a19c
mrinaldey <mrinalsdey33@gmail.com>
2021-12-09 13:24:18 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-let counter = 0;
-let show = () => {
-  counter++;
-  const el = <h1>My love for you increases like:{counter}</h1>;
-  ReactDOM.render(el, document.getElementById('root'));
-};
-setInterval(show, 500);
-
-class Confess extends React.Component {
-  render() {
-    return <h1> I love you babie </h1>;
-  }
+function Item(props) {
+  return (
+    <div className="item">
+      <b>Name:</b>
+      {props.name}
+      <br />
+      <b>Price:</b>
+      {props.price}
+    </div>
+  );
 }
-const dum = <Confess />;
-ReactDOM.render(dum, document.getElementById('chapri'));
-
-function Hello(props) {
-  return <p>Hello, {props.name}!</p>;
-}
-
-const el = <Hello name="HOOMAN" />;
-ReactDOM.render(el, document.getElementById('ello'));
+const el = <Item name="Cheese" price="4.99" /> 
+ReactDOM.render(
+  el,
+  document.getElementById('chapri')
+)
